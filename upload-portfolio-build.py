e603d35bb170ee0de37709d54968e0588f2b2229
Vikas Bhatia <vikbht@gmail.com>
2019-03-06 20:56:24 -0500

    Conforming to lambda handler spec

--- upload-portfolio-build.py
+++ upload-portfolio-build.py
@@ -1,20 +1,26 @@
+import json
 import boto3
 from botocore.client import Config
 import io
 import zipfile
 import mimetypes
 
-s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-porfolio_bucket = s3.Bucket('pfvb')
-porfolio_bucket_build = s3.Bucket('pfvb-build')
-
-portfolio_zip = io.BytesIO()
-
-porfolio_bucket_build.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        porfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        porfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+    porfolio_bucket = s3.Bucket('pfvb')
+    porfolio_bucket_build = s3.Bucket('pfvb-build')
+    
+    portfolio_zip = io.BytesIO()
+    
+    porfolio_bucket_build.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            porfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            porfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            
+    return 'Lambda invoked'
