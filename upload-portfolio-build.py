88d9f7c88a1494ea234db0c3547a219908b823f1
Vikas Bhatia <vikbht@gmail.com>
2019-03-06 21:12:34 -0500

    Updated lambda function with pub-sub capability

--- upload-portfolio-build.py
+++ upload-portfolio-build.py
@@ -7,20 +7,27 @@ import mimetypes
 
 def lambda_handler(event, context):
 
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:831010282937:deployPortfolioTopic')
     
-    s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
+    try:
+        s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
     
-    porfolio_bucket = s3.Bucket('pfvb')
-    porfolio_bucket_build = s3.Bucket('pfvb-build')
-    
-    portfolio_zip = io.BytesIO()
-    
-    porfolio_bucket_build.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            porfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            porfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-            
+        porfolio_bucket = s3.Bucket('pfvb')
+        porfolio_bucket_build = s3.Bucket('pfvb-build')
+        
+        portfolio_zip = io.BytesIO()
+        
+        porfolio_bucket_build.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                porfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                porfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployment successful") 
+    except:
+        topic.publish(Subject="Portfolio Deployment failue", Message="Portfolio deployment failue") 
+        raise
+        
     return 'Lambda invoked'
